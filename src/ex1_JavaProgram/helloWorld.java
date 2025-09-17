27a86c0caa131ff296b1c45f9953098528fa501b
VadakuppaPavithra <pavithrayadav652@gmail.com>
2025-09-17 08:48:04 +0530

    Basic verifications

--- src/ex1_JavaProgram/helloWorld.java
+++ src/ex1_JavaProgram/helloWorld.java
@@ -2,7 +2,10 @@ package ex1_JavaProgram;
 
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(2+2);
+        System.out.println("2x2");
+        System.out.println("Hello world");
+
     }
 }
 
---
+++ src/ex_4_JavaProgram/Lab004.java
@@ -0,0 +1,7 @@
+package ex_4_JavaProgram;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        System.out.println("hi Pavithra");
+    }
+}
